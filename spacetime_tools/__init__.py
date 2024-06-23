5ab581aef1fdb557717e637bc82920de1f00fcc9
Siddhant Gupta <siddhantgupta3@gmail.com>
2024-06-24 00:04:22 +0100

    base commit

---
+++ spacetime_tools/__init__.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
+
+logging.basicConfig(level=LOG_LEVEL)
+
+logging.addLevelName(logging.INFO, "info")
+logging.addLevelName(logging.ERROR, "error")
+logging.addLevelName(logging.DEBUG, "debug")
+logging.addLevelName(logging.CRITICAL, "critical")
+
+logging.getLogger().handlers.clear()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    "[%(asctime)s] - [%(name)s:%(lineno)d] - [%(levelname)s] - %(message)s"
+)
+
+handler.setFormatter(formatter)
+logger.addHandler(handler)
