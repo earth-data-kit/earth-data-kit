a48a513ee44cbbd8d7008c053d78fae8af8988f4
Siddhant Gupta <siddhantgupta3@gmail.com>
2024-07-24 00:54:57 +0100

    added no-sign-request, request-payer, aws-profile flags

--- spacetime_tools/stitching/engines/s3.py
+++ spacetime_tools/stitching/engines/s3.py
@@ -5,9 +5,28 @@ import spacetime_tools.stitching.helpers as helpers
 
 logger = logging.getLogger(__name__)
 
+
 class S3:
     def __init__(self, options) -> None:
+        no_sign_flag = ""
+        request_payer_flag = ""
+        profile_flag = ""
+        json_flag = "--json"
+
         self.options = options
+
+        if ("no_sign_request" in self.options) and (self.options["no_sign_request"]):
+            no_sign_flag = "--no-sign-request"
+
+        if ("request_payer" in self.options) and (self.options["request_payer"]):
+            request_payer_flag = f"--request-payer {self.options['request_payer']}"
+
+        if "profile" in self.options:
+            profile_flag = f"--profile {self.options['profile']}"
+
+        self.base_cmd = (
+            f"s5cmd {no_sign_flag} {request_payer_flag} {profile_flag} {json_flag}"
+        )
 
     def create_inventory(self, patterns, tmp_base_dir):
         # TODO: Add optimization to search till common first wildcard and filter them later
@@ -23,7 +42,7 @@ class S3:
         fp.writelines(cmds)
         fp.close()
 
-        s5_cmd = f"s5cmd --no-sign-request --json run {ls_cmds_fp} > {inventory_fp}"
+        s5_cmd = f"{self.base_cmd} run {ls_cmds_fp} > {inventory_fp}"
         os.system(s5_cmd)
 
         df = pd.read_json(inventory_fp, lines=True)
@@ -33,7 +52,6 @@ class S3:
         df["engine_path"] = df["key"]
 
         return df[["engine_path", "gdal_path", "size"]]
-
 
     def sync_inventory(self, df, tmp_base_dir):
         # Deleting /raw dir where data will be synced
@@ -46,7 +64,7 @@ class S3:
         cmds_fp = f"{tmp_base_dir}/sync_commands.txt"
 
         cmds.to_csv(cmds_fp, header=False, index=False)
-        s5_cmd = f"s5cmd --no-sign-request run {cmds_fp}"
+        s5_cmd = f"{self.base_cmd} run {cmds_fp}"
 
         os.system(s5_cmd)
 
