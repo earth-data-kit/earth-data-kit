337e0bc68dd8c6d4b1bb318ad91c4e5545aa4ce2
Siddhant Gupta <siddhantgupta3@gmail.com>
2025-05-08 14:11:24 +0530

    wip: benchmarks

--- benchmarks/read_edk.py
+++ benchmarks/read_edk.py
@@ -1,21 +1,8 @@
 import xarray as xr
-from earth_data_kit.stitching.decorators import log_time, log_init
+import earth_data_kit as edk
 
 
-@log_init
-@log_time
-def read_edk(vrt_path, block_multiplier):
-    block_size = 128
-    ds = xr.open_dataset(
-        vrt_path,
-        engine="edk_dataset",
-        chunks={
-            "time": 1,
-            "band": "auto",
-            "x": block_size * block_multiplier,
-            "y": block_size * block_multiplier,
-        },
-    )
-
-    res = ds.sel(time="2017-01-01").mean().compute()
-    print(res)
+def read_edk(json_path):
+    da = edk.stitching.Dataset.dataarray_from_file(json_path)
+    res = da.sel(time="2017-01-01", band=1).mean().compute()
+    return res
